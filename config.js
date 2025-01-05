cf13febaf719f89054973955805f8157929b7756
Samuel-Muli <salmmuelmully@gmail.com>
2025-01-05 13:23:04 +0300

    tulips

--- config.js
+++ config.js
@@ -5,14 +5,14 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ciwwAIoQ#0RYw-yNj-dC8xZBWCE5c8yfA5Ih9_wmNIIj6Y5fwtLE",
+SESSION_ID: process.env.SESSION_ID || "kvgXmDqZ#wHiPGm99_sRncTwDI1kX1xQXIaQl9_aTSDb3wVl0PQs",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://qu.ax/SnaGx.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR\n IM ONLINE\n\n> SaMu",
+ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR\n IM ONLINE\n\n> The-Great",
 ANTI_LINK: process.env.ANTI_LINK || "false",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 PREFIX: process.env.PREFIX || "/",
